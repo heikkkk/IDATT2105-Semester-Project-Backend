bd2ddbc9f8ab045816b4fcfdc72658ee3db5f63b
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-03-26 19:24:32 +0100

    Refactor: removed userId

--- src/main/java/no/ntnu/idi/idatt2105/quizopia/backend/model/User.java
+++ src/main/java/no/ntnu/idi/idatt2105/quizopia/backend/model/User.java
@@ -2,7 +2,6 @@ package no.ntnu.idi.idatt2105.quizopia.backend.model;
 
 public class User {
 
-    private Long user_id;
     private String username;
     private String email;
     private String password;
@@ -12,8 +11,7 @@ public class User {
     public User() {
     }
 
-    public User(Long user_id, String username, String email, String password, Long role_id) {
-        this.user_id = user_id;
+    public User(String username, String email, String password, Long role_id) {
         this.username = username;
         this.email = email;
         this.password = password;
@@ -21,13 +19,6 @@ public class User {
     }
 
     // Getters and Setters
-    public Long getUserId() {
-        return user_id;
-    }
-
-    public void setUserId(Long user_id) {
-        this.user_id = user_id;
-    }
 
     public String getUsername() {
         return username;
@@ -64,7 +55,6 @@ public class User {
     @Override
     public String toString() {
         return "Users{" +
-                "user_id=" + user_id +
                 ", name='" + username + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
