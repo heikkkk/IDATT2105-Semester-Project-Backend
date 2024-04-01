a9fc913857a02890c96ca180931b82fdaba4a432
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-04-01 12:13:04 +0200

    Add JavaDoc for UserMapper

--- src/main/java/no/ntnu/idi/idatt2105/quizopia/backend/service/UserMapper.java
+++ src/main/java/no/ntnu/idi/idatt2105/quizopia/backend/service/UserMapper.java
@@ -6,12 +6,20 @@ import no.ntnu.idi.idatt2105.quizopia.backend.model.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+/**
+ * Implementation of logout functionality for revoking refresh tokens and clearing the refresh token cookie.
+ */
 @Component
 @RequiredArgsConstructor
 public class UserMapper {
 
   private final PasswordEncoder passwordEncoder;
 
+  /**
+   * Maps a {@link UserRegistrationDto} object to a {@link User} object
+   * @param userRegistrationDto to be mapped
+   * @return the mapped {@link User} object
+   */
   public User toUser(UserRegistrationDto userRegistrationDto) {
     User user = new User();
     user.setUsername(userRegistrationDto.username());
