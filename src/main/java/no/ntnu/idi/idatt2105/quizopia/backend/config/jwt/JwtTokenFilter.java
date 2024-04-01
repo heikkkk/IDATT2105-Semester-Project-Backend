f7faac8caf0907834b4bedf40d23b770582f0607
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-04-01 12:12:27 +0200

    Add JavaDoc for JWT-token config

--- src/main/java/no/ntnu/idi/idatt2105/quizopia/backend/config/jwt/JwtTokenFilter.java
+++ src/main/java/no/ntnu/idi/idatt2105/quizopia/backend/config/jwt/JwtTokenFilter.java
@@ -23,6 +23,9 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.server.ResponseStatusException;
 
+/**
+ * Filter to intercept and process JWT tokens in HTTP requests.
+ */
 @RequiredArgsConstructor
 @Slf4j
 public class JwtTokenFilter extends OncePerRequestFilter {
@@ -30,12 +33,39 @@ public class JwtTokenFilter extends OncePerRequestFilter {
   private final RSAKeyRecord rsaKeyRecord;
   private final JwtTokenUtils jwtTokenUtils;
 
+  /**
+   * Filters incoming HTTP requests, intercepts JWT tokens, and processes them.
+   * The method first checks the Authorization header of the request to determine if it contains a JWT token.
+   * <br>
+   * If it does, the JWT token is extracted and decoded using the provided RSA public key.
+   * <br>
+   * Then, the username is extracted from the decoded JWT token.
+   * <br>
+   * If the username is not empty and there is no existing authentication in the
+   * {@link SecurityContext},
+   * {@link UserDetails} are retrieved for the username, and the token's validity is checked.
+   * <br>
+   * If the token is valid, a new authentication token is created and set in the
+   * {@link SecurityContext}.
+   * <br>
+   * Finally, the filter chain continues to process the request.
+   * <br>
+   * If JWT validation fails, a NOT_ACCEPTABLE status response is thrown.
+   *
+   * @param request The HTTP servlet request.
+   * @param response The HTTP servlet response.
+   * @param filterChain The filter chain.
+   * @throws ServletException If a servlet-related exception occurs.
+   * @throws IOException If an I/O exception occurs.
+   */
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
     try {
       final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
       log.info("[JwtTokenFilter:doFilterInterval] authHeader: {}", authHeader);
+
       JwtDecoder jwtDecoder = NimbusJwtDecoder.withPublicKey(rsaKeyRecord.rsaPublicKey()).build();
 
       if (!authHeader.startsWith(TokenType.Bearer.name())) {
@@ -45,7 +75,9 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
       final String token = authHeader.substring(7);
       final Jwt jwtToken = jwtDecoder.decode(token);
+
       log.info("[JwtTokenFilter:doFilterInterval] jwtToken: {}", jwtToken);
+
       final String username = jwtTokenUtils.getUsername(jwtToken);
 
       if (!username.isEmpty() && SecurityContextHolder.getContext().getAuthentication() == null) {
