a93f7247df9e219ff8b71d59bfcd8c6b5ce3211c
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-04-01 12:12:27 +0200

    Add JavaDoc for JWT-token config

--- src/main/java/no/ntnu/idi/idatt2105/quizopia/backend/config/jwt/JwtRefreshTokenFilter.java
+++ src/main/java/no/ntnu/idi/idatt2105/quizopia/backend/config/jwt/JwtRefreshTokenFilter.java
@@ -23,6 +23,9 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.server.ResponseStatusException;
 
+/**
+ * Filter to intercept and process JWT refresh tokens in HTTP requests.
+ */
 @RequiredArgsConstructor
 @Slf4j
 public class JwtRefreshTokenFilter extends OncePerRequestFilter {
@@ -30,10 +33,37 @@ public class JwtRefreshTokenFilter extends OncePerRequestFilter {
   private final JwtTokenUtils jwtTokenUtils;
   private final JdbcRefreshTokenRepository refreshTokenRepository;
 
+  /**
+   * Filters incoming HTTP requests, intercepts JWT refresh tokens, and processes them.
+   * The method first checks the Authorization header of the request to determine if it contains a JWT refresh token.
+   * <br>
+   * If it does, the JWT refresh token is extracted and decoded using the provided RSA public key.
+   * <br>
+   * Then, the username is extracted from the decoded JWT refresh token.
+   * <br>
+   * If the username is not empty and there is no existing authentication in the SecurityContext,
+   * {@link UserDetails} are retrieved for the username, and the token's validity is checked.
+   * <br>
+   * Additionally, the validity of the refresh token is checked in the database.
+   * <br>
+   * If both the refresh token and access token are valid, a new authentication token is created
+   * and set in the {@link SecurityContext}.
+   * <br>
+   * Finally, the filter chain continues to process the request.
+   * <br>
+   * If JWT validation fails, a NOT_ACCEPTABLE status response is thrown.
+   *
+   * @param request The HTTP servlet request.
+   * @param response The HTTP servlet response.
+   * @param filterChain The filter chain.
+   * @throws ServletException If a servlet-related exception occurs.
+   * @throws IOException If an I/O exception occurs.
+   */
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
     try {
+
       log.info("[JwtRefreshTokenFilter::doFilterInternal]Filtering Http request: {}",
           request.getRequestURI());
 
@@ -74,10 +104,13 @@ public class JwtRefreshTokenFilter extends OncePerRequestFilter {
 
       }
       log.info("[JwtRefreshTokenFilter::doFilterInternal]Complete");
+
       filterChain.doFilter(request, response);
     } catch (JwtValidationException jwtValidationException) {
+
       log.info("[JwtRefreshTokenFilter::doFilterInternal]Exception due to: {}",
           jwtValidationException.getMessage());
+
       throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE,
           jwtValidationException.getMessage());
     }
