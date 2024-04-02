a214f4ab0868dff8e6472c1ebf01d7885f29eaec
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-04-02 09:16:10 +0200

    Updated sql-schema scripts

--- src/test/java/no/ntnu/idi/idatt2105/quizopia/backend/QuizopiaApplicationTests.java
+++ src/test/java/no/ntnu/idi/idatt2105/quizopia/backend/QuizopiaApplicationTests.java
@@ -2,8 +2,10 @@ package no.ntnu.idi.idatt2105.quizopia.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest()
+@TestPropertySource(locations="classpath:application-test.properties")
 class QuizopiaApplicationTests {
 
 	@Test
