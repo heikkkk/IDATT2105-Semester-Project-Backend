c2eb9390f3d18c29149a1cdfebef3bb751d36ada
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-04-03 11:39:43 +0200

    Fix bug in test tyring to POST to GET-mapping

--- src/test/java/no/ntnu/idi/idatt2105/quizopia/backend/controller/UserControllerTest.java
+++ src/test/java/no/ntnu/idi/idatt2105/quizopia/backend/controller/UserControllerTest.java
@@ -33,7 +33,7 @@ class UserControllerTest {
   @Test
   @WithMockUser
   void shouldGetAdminUserIdByUserName() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.post("/api/users/get-id/adminUser"))
+    mockMvc.perform(MockMvcRequestBuilders.get("/api/users/get-id/adminUser"))
         .andExpect(MockMvcResultMatchers.content().string("1"))
         .andExpect(MockMvcResultMatchers.status().isOk());
   }
