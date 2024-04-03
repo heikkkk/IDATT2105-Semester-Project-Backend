915cabb755741ca48b80b3086c853f8d17862207
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-04-03 09:26:20 +0200

    Tests for retrieving id given username

--- src/test/java/no/ntnu/idi/idatt2105/quizopia/backend/controller/UserControllerTest.java
+++ src/test/java/no/ntnu/idi/idatt2105/quizopia/backend/controller/UserControllerTest.java
@@ -1,9 +1,12 @@
 package no.ntnu.idi.idatt2105.quizopia.backend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import no.ntnu.idi.idatt2105.quizopia.backend.dto.UsernameRequestDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,10 +21,25 @@ class UserControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
+  @Autowired
+  ObjectMapper objectMapper;
+
   @Test
   @WithMockUser
   void shouldGetUsernameAdminUserByUserId() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.get("/api/users/get-name/1"))
-        .andExpect(MockMvcResultMatchers.content().string("adminUser"));
+        .andExpect(MockMvcResultMatchers.content().string("adminUser"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  @WithMockUser
+  void shouldGetAdminUserIdByUserName() throws Exception {
+    UsernameRequestDto requestDto = new UsernameRequestDto("adminUser");
+    mockMvc.perform(MockMvcRequestBuilders.post("/api/users/get-id")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(requestDto)))
+        .andExpect(MockMvcResultMatchers.content().string("1"))
+        .andExpect(MockMvcResultMatchers.status().isOk());
   }
 }
