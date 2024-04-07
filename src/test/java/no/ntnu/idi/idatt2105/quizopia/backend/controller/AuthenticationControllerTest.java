365c53854e02d01d0a2beec76876606c7e9b9757
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-04-07 22:24:56 +0200

    Removed refreshTokens tests from the backend

--- src/test/java/no/ntnu/idi/idatt2105/quizopia/backend/controller/AuthenticationControllerTest.java
+++ src/test/java/no/ntnu/idi/idatt2105/quizopia/backend/controller/AuthenticationControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,19 +31,6 @@ class AuthenticationControllerTest {
 
   @Autowired
   private ObjectMapper objectMapper;
-
-  private MvcResult generateAuthResponse() throws Exception {
-    String username = "adminUser";
-    String password = "password123";
-    String credentials = username + ":" + password;
-    String base64Credentials = new String(Base64.getEncoder().encode(credentials.getBytes()));
-
-    return mockMvc.perform(MockMvcRequestBuilders.post("/sign-in")
-            .contentType(MediaType.APPLICATION_JSON)
-            .header(HttpHeaders.AUTHORIZATION, "Basic " + base64Credentials))
-        .andReturn();
-  }
-
   @Test
   void shouldAuthenticateUserAndGenerateAccessToken() throws Exception {
 
@@ -60,23 +46,7 @@ class AuthenticationControllerTest {
        .andExpect(jsonPath("$.access_token").exists())
        .andExpect(jsonPath("$.access_token_expiry").exists())
        .andExpect(jsonPath("$.token_type").exists())
-       .andExpect(jsonPath("$.user_name").exists())
-       .andExpect(MockMvcResultMatchers.cookie().exists("refresh_token"));
-  }
-
-  @Test
-  void shouldGetAccessTokenFromRefreshToken() throws Exception {
-    MvcResult authResponse = generateAuthResponse();
-    String refreshToken = authResponse.getResponse().getCookie("refresh_token").getValue();
-
-    mockMvc.perform(MockMvcRequestBuilders.post("/refresh-token")
-            .contentType(MediaType.APPLICATION_JSON)
-            .header(HttpHeaders.AUTHORIZATION, "Bearer " + refreshToken))
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.jsonPath("$.access_token").exists())
-        .andExpect(jsonPath("$.access_token_expiry").exists())
-        .andExpect(jsonPath("$.token_type").exists())
-        .andExpect(jsonPath("$.user_name").exists());
+       .andExpect(jsonPath("$.user_name").exists());
   }
 
   @Test
